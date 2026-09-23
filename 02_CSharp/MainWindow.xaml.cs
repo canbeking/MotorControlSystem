3deae6071cf41ce0de3f1c76031902caa67b9ed3
Jack <canbeking@hotmail.com>
2026-09-23 19:25:50 +0800

    Add CSharp files

---
+++ 02_CSharp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using MotorControlSystem.ViewModels;
+using MotorControlSystem.Services;
+
+
+namespace MotorControlSystem
+{
+    public partial class MainWindow : Window
+    {
+        private readonly PlcService _plcService = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            DataContext = new MainViewModel();
+
+            TestPlcConnection();
+
+        }
+
+        private void TestPlcConnection()
+        {
+            bool connected = _plcService.Connect();
+
+            //MessageBox.Show(
+            //    connected
+            //        ? "PLC连接成功！"
+            //        : "PLC连接失败！",
+            //    "PLC通信测试");
+        }
+
+        private async void Click_PLC_Start(object sender, RoutedEventArgs e)
+        {
+            await _plcService.StartAsync();
+        }
+    }
+}
